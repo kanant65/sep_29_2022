8fe39fdbebb603f50325f702a989541f2db320bf
kanant65 <86822605+kanant65@users.noreply.github.com>
2022-09-29 14:20:16 +0530

    Update Ques001.java

--- Ques001.java
+++ Ques001.java
@@ -1,33 +1,39 @@
-package javatest;
+package com.javatest.bll;
 import java.util.LinkedList;
 
 
 public class Ques001 {
 
 	public static void main(String[] args) {
-		LinkedList<String> language=new LinkedList<String>();
-		language.add("C");
-		language.add("C++");
-		language.add("Java");
-		language.add("Kotlin");
-		language.add("Python");
-		language.add("Perl");
-		language.add("Ruby");
+		LinkedList<String> languages=new LinkedList<String>();
+		languages.add("C");
+		languages.add("C++");
+		languages.add("Java");
+		languages.add("Kotlin");
+		languages.add("Python");
+		languages.add("Perl");
+		languages.add("Ruby");
 		
-		System.out.println(language);
+		System.out.println(languages);
+		/*
+		languages.remove(5);
+		languages.remove("Kotlin");
 		
-		language.remove(5);
-		language.remove("Kotlin");
+		System.out.println("after removing 5th and Kotlin   :"+languages);
+		*/
+		/*LinkedList<String> scriptinglanguages = new LinkedList<>();
+		scriptinglanguages.add("Python");
+		scriptinglanguages.add("Ruby");
+		scriptinglanguages.add("Perl");
 		
-		System.out.println("after removing 5th and Kotlin   :"+language);
+		System.out.println("Scripting languagess : " +scriptinglanguages);
+		languages.removeAll(scriptinglanguages);
 		
-		language.remove("Python");
+		System.out.println("after Removing all Scripting languages   :"+languages);*/
+		/*
+		languages.clear();
 		
-		System.out.println("after Removing all Scripting languages   :"+language);
-		
-		language.clear();
-		
-		System.out.print("after removing all the elements from list   :"+language);
+		System.out.print("after removing all the elements from list   :"+languages);*/
 		
 		
 		
